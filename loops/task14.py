b89c4f49605aa21465c17bfdb77adfee588426d8
elsenismayil <elsenismayilov0057@gmail.com>
2026-07-19 14:57:03 +0300

    task 14 solved

---
+++ loops/task14.py
@@ -0,0 +1,17 @@
+nums = int(input("enter num"))
+for i in range(1,(nums+1)):
+    if i == 1 or i ==nums:
+       continue
+    elif nums%i==0:
+        print(f"{nums} is no primary num")
+        break
+    else:
+        print(f"{nums} is primary num")
+        break
+    
+   
+
+
+    
+
+
