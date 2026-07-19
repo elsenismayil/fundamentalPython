013aa1d5e7dab5bdf11dd1bad3b5421fc3aec996
elsenismayil <elsenismayilov0057@gmail.com>
2026-07-19 14:08:12 +0300

    task 12 solved

---
+++ loops/task11.py
@@ -0,0 +1,18 @@
+i =1
+while i<100:
+    if i %2==0:
+     print(i)
+    i+=1
+
+k=1
+while k<100:
+   if k%2!=0:
+    k+=1
+    continue
+   print(k)
+   k+=1
+
+
+
+
+
---
+++ loops/task12.py
@@ -0,0 +1,8 @@
+sum = 0
+while True:
+ ent = int(input("enter"))
+ sum+=ent
+ if ent ==0:
+  print(f"cem: {sum}")
+  break
+ 
