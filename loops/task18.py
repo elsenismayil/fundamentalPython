df735b4912b65225fa5ce4e24b49fb17a3273df2
elsenismayil <elsenismayilov0057@gmail.com>
2026-07-20 21:54:36 +0300

    task 18 solved

--- loops/task18.py
+++ loops/task18.py
@@ -5,4 +5,12 @@ for i in range(1,b+1):
     faktoryal = faktoryal*i
     a.append(faktoryal)
 print(a[-1])
+# *******************************
+ent =int(input("enter"))
+f = 1
+for k in range(1,ent+1):
+    f = f*k
+
+print(f"{ent}!={f}")
+
 
