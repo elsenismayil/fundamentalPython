432ad1edcd90ab3cc9b8a4cfdfb5eb43d1a415c6
elsenismayil <elsenismayilov0057@gmail.com>
2026-07-20 22:22:33 +0300

    task 14 changed

--- loops/task14.py
+++ loops/task14.py
@@ -9,7 +9,16 @@ for i in range(1,(nums+1)):
         print(f"{nums} is primary num")
         break
     
-   
+a =int(input("enter num"))
+prime = True
+for z in range(2,a):
+    if a%z==0:
+        prime=False
+        break
+if prime is True:
+    print("primary")
+else:
+    print("no primary")
 
 
     
