1ab4838ef75eccd23ca21b8b4d51ff01b166e3cf
elsenismayil <elsenismayilov0057@gmail.com>
2026-07-28 13:05:53 +0300

    list cleared by reverse loop

--- common/copylist.py
+++ common/copylist.py
@@ -1,4 +1,11 @@
-a = [2, 3, 4, 5, 6, 7, 8]
-for x in a[:]:
-    a.remove(x)
-print(a)
+#copied list
+b = [2, 3, 4, 5, 6, 7, 8]
+for x in b[:]:
+    b.remove(x)
+print(b)
+#************************************
+#reverse loop
+b = [2, 3, 4, 5, 6, 7, 8]
+for t in range(len(b)-1,-1,-1):
+    b.remove(b[t])
+print(b)
